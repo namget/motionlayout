13d31177534cf7557d11617e4c6781992b027073
leejaewoo <yeboykr@neighbor21.co.kr>
2019-04-12 10:38:01 +0900

    first commit

---
+++ app/src/main/java/com/namget/motionlayouttest/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.namget.motionlayouttest
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_motion)
+
+
+    }
+}
+
